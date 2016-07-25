f956b46335cd54fe04f7af644137a83e5ffb67a2
Danny Wilkins <daniel.wilkins@nyu.edu>
2016-07-25 13:39:40 -0400

    Added read_partitions. Reads /proc/partitions and outputs parition names and sizes.

--- proc.py
+++ proc.py
@@ -1,17 +1,24 @@
 import sys
 
 def main():
+	if len(sys.argv) < 2:
+		print("Usage: python proc.py [meminfo] [loadavg] [-b] [-v]")
+		sys.exit()
+
 	if 'meminfo' in sys.argv:
 		read_meminfo()
 
 	if 'loadavg' in sys.argv:
 		read_loadavg()
 
+	if 'partitions' in sys.argv:
+		read_partitions()
+
 def read_meminfo():
-	if '-b' in sys.argv:
-		style = 'b'
-	elif '-v' in sys.argv:
+	if '-v' in sys.argv:
 		style = 'v'
+	elif '-b' in sys.argv:
+		style = 'b'
 	else:
 		style = input("Would you like to read meminfo as [b]asic or [v]erbose?\n").lower()
 
@@ -29,10 +36,10 @@ def read_meminfo():
 			read_meminfo()
 
 def read_loadavg():
-	if '-b' in sys.argv:
-		style = 'b'
-	elif '-v' in sys.argv:
+	if '-v' in sys.argv:
 		style = 'v'
+	elif '-b' in sys.argv:
+		style = 'b'
 	else:
 		style = input("Would you like to read loadavg as [b]asic or [v]erbose?\n").lower()
 
@@ -52,11 +59,45 @@ def read_loadavg():
 			print("Argument not found. ")
 			read_loadavg()
 
+def read_stat():
+	pass
+
+def read_partitions():
+	if '-v' in sys.argv:
+		style = 'v'
+	elif '-b' in sys.argv:
+		style = 'b'
+	else:
+		style = input("Would you like to read partitions as [b]asic or [v]erbose?\n").lower()
+
+	with open('/proc/partitions', 'r') as partitions:		
+		if style == 'b':
+			data = partitions.readline().split(' ')
+			print('Name', '\t\t', 'Size', '\n', end='')
+			for line in partitions:
+				try:
+					print(line.split(' ')[-1].strip('\n'), '\t\t',\
+						kb_to_gb(int(line.split(' ')[-2])))
+				except:
+					pass
+
+		if style == 'v':
+			print(partitions.readline(), end='')
+			for line in partitions:
+				print(line, end='')
+
+
 def gb_to_kb(gb):
 	return "{:.0f} KB".format(float(gb)*1048576)	
 
 def kb_to_gb(kb):
 	return "{:.4f} GB".format(float(kb)/1048576)
+
+def kb_to_mb(kb):
+	return "{:.2f} MB".format(float(kb)/1024)
+
+def mb_to_kb(mb):
+	return "{:.3f} KB".format(float(mb)*1024)
 
 def mb_to_gb(mb):
 	return "{:.4f} GB".format(float(mb)/1024)
