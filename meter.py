7ff3a98594903bfd84a6d7fcb6e55c79d26ec697
Danny Wilkins <danny@localhost.localdomain>
2016-08-01 15:33:32 -0400

    Fixed RAM meter to accurately reflect available RAM

--- meter.py
+++ meter.py
@@ -4,12 +4,15 @@ from proc import *
 while True:
 	subprocess.call(['clear'])
 	loadavg_reading = int(read_loadavg())
-	print("[", "\033[1;33m|\033[0m"*loadavg_reading, "-"*(100-loadavg_reading), "]\n", sep='')
+	loadavg_used = "\033[1;33m|\033[0m"*loadavg_reading
+	loadavg_free = "-"*(100-loadavg_reading)
+	print("[", loadavg_used, loadavg_free, "]\n", sep='')
 	meminfo_reading = read_meminfo()
 	meminfo_reading = [int(i) for i in meminfo_reading]
-	print(100-round((meminfo_reading[1]/meminfo_reading[0])*100), "%", sep='')
-	print("[", "\033[1;33m|\033[0m"*(100-round((meminfo_reading[1]/meminfo_reading[0])*100)), \
-		"-"*(round((meminfo_reading[1]/meminfo_reading[0])*100)), "]", sep='')
+	meminfo_used = 100-round((meminfo_reading[2]/meminfo_reading[0])*100)
+	meminfo_free = "-"*(round((meminfo_reading[2]/meminfo_reading[0])*100))
+	print(meminfo_used, "%", sep='')
+	print("[", "\033[1;33m|\033[0m"*meminfo_used, meminfo_free, "]", sep='')
 	time.sleep(1)
 	
 
